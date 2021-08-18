164b55a928ecc913c89e132b6e81077705c58dd8
nicholascfischer <nicholascfischer@gmail.com>
2021-08-18 15:32:34 -0500

    Committed

--- app.js
+++
@@ -1,4 +0,0 @@
-const menu = document.queryselector('#mobile-menu')
-const menuLink = document.queryselector()
-
-
---
+++ public/app.js
@@ -0,0 +1,9 @@
+const menu = document.queryselector('#mobile-menu')
+const menuLink = document.queryselector('.navbar__menu')
+
+menu.addEventListener('click', funchtion() {
+	menu.classList('is-active')
+	menuLinks.classList.toggle('active')
+})
+
+
